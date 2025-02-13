86fb8a10deff042e5714b786426d6ef5d3feec78
NishaM2 <nisham2522@gmail.com>
2025-02-13 12:51:20 +0530

    implemented middlewares

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 const express = require("express");
 const app = express();
 
-app.get("/health-checkup",(req,res) => {
+function userMiddleware(req, res, next) {
     const username = req.headers.username;
     const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
 
     if(username != "Nisha" || password != "pass"){
-        res.status(400).json({"msg" : "Somethings up with ur inputs"})
-        return
+        res.status(403).json({
+            msg : "incorrect inputs",
+        });
+    } else {
+        next();
     }
+};
 
+function kidneyMiddleware(req, res, next) {
+    const kidneyId = req.query.kidneyId;
+    
     if(kidneyId != 1 && kidneyId != 2) {
-        res.status(400).json({"msg" : "Somethings up with ur inputs"})
-        return
+        res.status(403).json({
+            msg : "incorrect inputs",
+        });
+        } else {
+            next();
     }
+};
 
-    res.json({
-        msg:"Your Kidney is fine!"
-    })
-    
-    
-  
+app.get("/health-checkup", userMiddleware, kidneyMiddleware, function (req, res) {
+    res.send("Your kidney is healthy");
+});
+
+app.get("/kidney-checkup", userMiddleware, kidneyMiddleware, function (req, res) {
+    res.send("Your kidney is healthy");
+});
+
+app.get("/heart-checkup", userMiddleware, function (req, res) {
+    res.send("Your heart is healthy");
 });
 
 app.listen(3000);
